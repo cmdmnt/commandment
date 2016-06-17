53b1ef5b7b0a9f603340492b153e25a7cbae92c6
Jesse Peterson <jesse@mactechs.com>
2016-06-17 12:19:45 -0700

    Fix ordering of profile prefix check

--- commandment/mdm.py
+++ commandment/mdm.py
@@ -122,11 +122,11 @@ def enroll():
 
     config = db_session.query(MDMConfig).first()
 
-    if not config.prefix:
-        abort(500, 'MDM configuration has no profile prefix')
-
     if not config:
         abort(500, 'No MDM configuration present; cannot generate enrollment profile')
+
+    if not config.prefix or not config.prefix.strip():
+        abort(500, 'MDM configuration has no profile prefix')
 
     profile = Profile(config.prefix + '.enroll', PayloadDisplayName=config.mdm_name)
 
