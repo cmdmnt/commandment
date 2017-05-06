0a1e85d0605f7c451e688136b0dd395da6370fbc
mosen <mosen1@gmail.com>
2017-05-06 17:13:28 +1000

    Remove unused scep test Remove unused mdm payload classes

--- tests/client.py
+++ tests/client.py
@@ -2,6 +2,14 @@ from flask.testing import FlaskClient
 
 
 class MDMClient(FlaskClient):
+    """MDMClient is a superset of the flask testing client meant to perform higher level operations similar to the
+    native mdmclient binary.
+    
+    Attributes:
+          _private_key (rsa.RSAPrivateKey): RSA Private Key for the simulated client.
+          _certificate (x509.Certificate): X.509 Certificate for the simulated client.
+    """
+    
     def __init__(self, *args, **kwargs):
         self._private_key = kwargs.get('private_key', None)
         self._certificate = kwargs.get('certificate', None)
