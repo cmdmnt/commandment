dbb9f809f6a9da13ac93c013d15454cdabba849b
Mosen <mosen@users.noreply.github.com>
2017-05-08 21:31:28 +1000

    Ignore local Alembic config

--- commandment/alembic/env.py
+++ commandment/alembic/env.py
@@ -15,7 +15,9 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from commandment.models import Device
+target_metadata = Device.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
