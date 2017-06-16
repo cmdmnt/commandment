e35bb41573efb8f539ee3d94d9f22474a619a61c
mosen <mosen@fastmail.com>
2017-06-16 21:50:54 +1000

    Fix webpack prod config. Created uwsgi-nginx docker config based on tiangolo/uwsgi-nginx-flask container.

--- ui/webpack.config.prod.js
+++ ui/webpack.config.prod.js
@@ -2,65 +2,83 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractSass = new ExtractTextPlugin({
-  filename: "css/[name].css"
-  //disable: process.env.NODE_ENV === "development"
+    filename: "css/[name].css"
 });
 
 
 module.exports = {
-  entry: {
-    app: [
-      'react-hot-loader/patch',
-      './src/entry.tsx'
+    entry: {
+        app: [
+            'react-hot-loader/patch',
+            './src/entry.tsx'
+        ]
+    },
+
+    output: {
+        path: path.resolve(__dirname, "..", "commandment", "static"),
+        filename: 'app.js',
+        publicPath: '/static/'
+    },
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+
+    devtool: 'cheap-module-eval-source-map',
+    target: 'web',
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['awesome-typescript-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'resolve-url-loader'
+                    }, {
+                        loader: 'sass-loader?sourceMap'
+                    }],
+                    fallback: 'style-loader'
+                })
+            },
+            {
+                test: /\.css$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'resolve-url-loader'
+                    }]
+                })
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: '[name]-[hash].[ext]',
+                        outputPath: 'images/'
+                    }
+                }]
+            },
+            {
+                test: /\.(ttf|eot|svg|woff|woff2)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts/'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        extractSass
     ]
-  },
-
-  output: {
-    path: path.resolve(__dirname, "..", "commandment", "static"),
-    filename: 'app.js',
-    publicPath: '/static/'
-  },
-
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-
-  devtool: 'cheap-module-eval-source-map',
-  target: 'web',
-
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: ['react-hot-loader/webpack', 'awesome-typescript-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: extractSass.extract({
-          use: [{
-            loader: 'css-loader'
-          },  {
-            loader: 'resolve-url-loader'
-          },  {
-            loader: 'sass-loader?sourceMap'
-          }],
-          fallback: 'style-loader'
-        })
-      },
-      {
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        use: ['file-loader?publicPath=fonts/&outputPath=fonts/']
-      }
-    ]
-  },
-
-  plugins: [
-    extractSass
-  ],
-  devServer: {
-    publicPath: "https://localhost:4000/static/",
-    hot: true,
-    port: 4000,
-    https: true
-  }
 };
