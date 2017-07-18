11bf0a7c5fb12a81be73bb9b9f10374b82dbfcb7
Mosen <mosen1@gmail.com>
2017-07-18 14:20:54 +1000

    More docblocks All tests for DEP failures on /session are passing Now re-throwing http errors as DEPError

--- commandment/dep/dep.py
+++ commandment/dep/dep.py
@@ -2,14 +2,20 @@ from typing import Union, List
 import requests
 from requests.auth import AuthBase
 from requests_oauthlib import OAuth1
-
+import re
+from datetime import timedelta
+from .errors import DEPError
 
 class DEPCursor:
     pass
 
 
 class DEPAuth(AuthBase):
-    """Attach X-ADM-Auth-Session token to the request"""
+    """Attach X-ADM-Auth-Session token to the request.
+
+    Example:
+          session.get("https://something", auth=DEPAuth(token))
+    """
     def __init__(self, token: str):
         self.token = token
 
@@ -45,14 +51,69 @@ class DEP:
         })
         self._token = None
 
-    def fetch_token(self, path: str = "/session") -> Union[str, None]:
-        res = self._session.get(self._url + path, auth=self._oauth)
+    def _set_token_from_header(self, r: requests.Response, *args, **kwargs):
+        """This method is added as a `response` hook to the request. If the service replies with a header
+        **X-ADM-Auth-Session**, then that token will replace the current token.
+
+        Args:
+              r (requests.Response): The response object
+
+        See Also:
+            - `Footnote about **X-ADM-Auth-Session** under Response Payload <https://developer.apple.com/library/content/documentation/Miscellaneous/Reference/MobileDeviceManagementProtocolRef/4-Profile_Management/ProfileManagement.html#//apple_ref/doc/uid/TP40017387-CH7-SW2>`_.
+        """
+        if 'X-ADM-Auth-Session' in r.headers:
+            self._token = r.headers['X-ADM-Auth-Session']
+
+    def _set_retry_after(self, r: requests.Response, *args, **kwargs):
+        """This method inspects the response headers for a `Retry-After` header."""
+        if 'Retry-After' in r.headers:
+            after = r.headers['Retry-After']
+            if re.compile(r"/[0-9]+").match(after):
+                d = timedelta(seconds=after)
+            else:  # HTTP Date
+                pass
+
+    def send(self, req: requests.Request, **kwargs) -> requests.Response:
+        """Send a request to the DEP service.
+
+        If the service responds that the token has expired, fetch a new session token and re-issue the request.
+
+        Args:
+              req (requests.Request): The request, which will have DEP auth headers added to it.
+        Returns:
+              requests.Response: The response
+        """
+        req.hooks = dict(response=self._set_token_from_header)
+        req.auth = DEPAuth(self._token)
+
+        prepared = self._session.prepare_request(req)
+
+        res = self._session.send(prepared, **kwargs)
+
+        try:
+            res.raise_for_status()
+        except requests.HTTPError as e:
+            raise DEPError(response=res, request=res.request) from e
+        
+        return res
+
+    def fetch_token(self) -> Union[str, None]:
+        """Request a new session token using our DEP credentials.
+
+        Returns:
+              Union[str, None]: The token that was returned (already set on this instance), or None if it failed.
+        """
+        res = self._session.get(self._url + "/session", auth=self._oauth)
+        try:
+            res.raise_for_status()
+        except requests.HTTPError as e:
+            raise DEPError(response=res, request=res.request) from e
 
         self._token = res.json().get("auth_session_token", None)
         return self._token
 
     def account(self, path: str = "/account") -> Union[None, dict]:
-        res = self._session.get(self._url + path, auth=DEPAuth(self._token))
+        res = self.send(requests.Request("GET", self._url + path))
         return res.json()
 
     def devices(self) -> DEPCursor:
--- commandment/dep/errors.py
+++ commandment/dep/errors.py
@@ -1,3 +1,22 @@
-class DEPSessionTokenExpired(Exception):
-    pass
+from requests import Response, HTTPError
 
+
+class DEPError(HTTPError):
+    """DEPError inherits from request's HTTPError to provide the response and request as part of the exception.
+
+    Additionally, the error tracks information about the body content as this can sometimes be the only way to
+    distinguish an error.
+
+    Attributes:
+          text (str): The reserved string that was returned in the error body.
+    """
+    def __init__(self, *args, **kwargs):
+        super(DEPError, self).__init__(*args, **kwargs)
+        if 'response' in kwargs:
+            # Quote characters (") must be stripped, because the body may contain the reason inside double quotes.
+            self.text = kwargs.get('response').content.strip('"')
+        else:
+            self.text = "NO_REASON_GIVEN"
+
+    def __str__(self):
+        return '{}: {}'.format(self.response.status_code, self.text)
