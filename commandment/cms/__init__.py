d97f12ea7b5006a7ef380e6666403f96b2c02cba
Mosen <mosen1@gmail.com>
2017-07-19 10:50:06 +1000

    Deal with several error cases in PKCS7/CMS decorator. Add signed data to g.signed_data in PKCS7/CMS decorator.

--- commandment/cms/__init__.py
+++ commandment/cms/__init__.py
@@ -1,5 +1,7 @@
-from typing import Union, Optional
-from asn1crypto.cms import CertificateSet, SignerIdentifier, Certificate
+from typing import Union, Optional, Type
+from asn1crypto.cms import CertificateSet, SignerIdentifier, Certificate, SignedDigestAlgorithm
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 
 def _certificate_by_signer_identifier(certificates: CertificateSet, sid: SignerIdentifier) -> Optional[Certificate]:
@@ -30,3 +32,43 @@ def _certificate_by_signer_identifier(certificates: CertificateSet, sid: SignerI
             return c
 
     return None
+
+
+def _cryptography_hash_function(algorithm: SignedDigestAlgorithm) -> Union[None, Type[hashes.SHA1], Type[hashes.SHA256], Type[hashes.SHA512]]:
+    """Find the cryptography hash function given the string output from asn1crypto SignedDigestAlgorithm.
+
+    Todo: There should be a better way to do this?
+
+    Args:
+          algorithm (SignedDigestAlgorithm): The asn1crypto Signed Digest Algorithm
+    Returns:
+        Union[Type[hashes.SHA1], Type[hashes.SHA256], Type[hashes.SHA512]] A cryptography hash function for use with
+         signature verification.
+    """
+
+    hash_algo = algorithm.hash_algo
+
+    if hash_algo == "sha1":
+        return hashes.SHA1
+    elif hash_algo == "sha256":
+        return hashes.SHA256
+    elif hash_algo == "sha512":
+        return hashes.SHA512
+    else:
+        return None
+
+
+def _cryptography_pad_function(algorithm: SignedDigestAlgorithm) -> Union[None, Type[padding.PKCS1v15]]:
+    """Find the cryptography pad function given a signed digest algorithm from asn1crypto.
+
+    Args:
+        algorithm (SignedDigestAlgorithm): The asn1crypto Signed Digest Algorithm
+    Returns:
+        Union[None, Type[padding.PKCS1v15]]: The padding function for the signed digest
+        """
+    signature_algo = algorithm.signature_algo
+
+    if signature_algo == "rsassa_pkcs1v15":
+        return padding.PKCS1v15
+    else:
+        return None
--- commandment/cms/decorators.py
+++ commandment/cms/decorators.py
@@ -1,21 +1,25 @@
 from flask import request, g
 from functools import wraps
 from cryptography import x509
-from cryptography.exceptions import UnsupportedAlgorithm, InvalidSignature
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding
 from asn1crypto import cms
-from . import _certificate_by_signer_identifier
+from . import _certificate_by_signer_identifier, _cryptography_hash_function, _cryptography_pad_function
 
 
 def verify_cms_signers(f):
     """Verify the signers of a request containing a CMS/PKCS#7, DER encoded body.
 
+    The certificate of each signer is placed on the global **g** variable as **g.signers**
+
+    Raises:
+          - TypeError if *Content-Type* header is not "application/pkcs7-signature"
+          - SigningError if any signer on the CMS content is not valid.
     """
     @wraps(f)
     def decorator(*args, **kwargs):
-        assert request.headers['Content-Type'] == "application/pkcs7-signature"
+        if request.headers['Content-Type'] != "application/pkcs7-signature":
+            raise TypeError("verify_cms_signers expects application/pkcs7-signature, got: {}".format(
+                request.headers['Content-Type']))
 
         ci = cms.ContentInfo.load(request.data)
         assert ci['content_type'].native == 'signed_data'
@@ -27,19 +31,12 @@ def verify_cms_signers(f):
             asn_certificate = _certificate_by_signer_identifier(signed['certificates'], signer['sid'])
             assert asn_certificate is not None
             certificate = x509.load_der_x509_certificate(signer.dump(), default_backend())
+
             signature_algorithm = signer['signature_algorithm']
-            signature_algo = signature_algorithm.signature_algo
-            hash_algo = signature_algorithm.hash_algo
-
-            hash_function = {
-                'sha1': hashes.SHA1,
-                'sha256': hashes.SHA256,
-                'sha512': hashes.SHA512
-            }.get(hash_algo, hashes.SHA1)
-
-            pad_function = {
-                'rsassa_pkcs1v15': padding.PKCS1v15
-            }.get(signature_algo, padding.PKCS1v15)
+            hash_function = _cryptography_hash_function(signature_algorithm)
+            pad_function = _cryptography_pad_function(signature_algorithm)
+            if hash_function is None or pad_function is None:
+                raise ValueError('Unsupported signature algorithm: {}'.format(signature_algorithm))
 
             verifier = certificate.public_key().verifier(
                 signer['signature'].native,
@@ -49,6 +46,9 @@ def verify_cms_signers(f):
             verifier.update(request.data)
             verifier.verify()  # Raises a SigningError if not valid
             g.signers.append(certificate)
+
+        # TODO: Don't assume that content is OctetString
+        g.signed_data = signed['encap_content_info']['content']
 
         return f(*args, **kwargs)
 
